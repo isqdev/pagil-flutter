8fe326ec415a6eae9533b3197182b550cdde1bb3
Isaque Dias <isaque.brain@gmail.com>
2025-06-12 20:13:00 -0300

    mudanca dir

--- lib/settings/routes.dart
+++ lib/settings/routes.dart
@@ -1,14 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:pagil_flutter/telas/form_banda_artista.dart';
 import 'package:pagil_flutter/telas/form_turma.dart';
-import 'telas/form_aluno.dart';
-import './telas/form_categoria_musica.dart';
-import './telas/form_fabricante.dart';
-import './telas/form_manutencao.dart';
-import './telas/form_sala.dart';
-import './telas/tela_dashboard.dart';
-import './telas/form_video_aula.dart';
-import './telas/lista_fabricante.dart';
+import '../telas/form_aluno.dart';
+import '../telas/form_categoria_musica.dart';
+import '../telas/form_fabricante.dart';
+import '../telas/form_manutencao.dart';
+import '../telas/form_sala.dart';
+import '../telas/tela_dashboard.dart';
+import '../telas/form_video_aula.dart';
+import '../telas/lista_fabricante.dart';
 
 
 class AppRoutes {
